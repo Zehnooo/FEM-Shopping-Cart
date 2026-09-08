4e4ba1363a293035b752996a311d0ff4e9171f88
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-08 16:24:35 -0500

    worked on cart - adding and removing items as well as calculating cart total upon update

--- src/script.js
+++ src/script.js
@@ -1,16 +1,7 @@
-import { getData } from './scripts/loadData.js';
-import { cart } from './scripts/cart.js';
-
+import {cart} from "./scripts/cart.js"
+import {getData} from "./scripts/loadData.js";
 (() => {
     console.log("init");
-    const items = getData();
-    console.log("1", cart.items);
-    cart.addItem(items[0]);
-    cart.addItem(items[1]);
-    cart.addItem(items[2]);
-    console.log("2", cart.items);
-    cart.addItem(items[0]);
-    cart.addItem(items[0]);
-    console.log("3", cart.items);
-    cart.removeItem(items[0]);
+    const data = getData();
+
 })();
--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,13 +1,26 @@
 export const cart= {
     items: [],
-    total: null,
+    total: 0,
 
     addItem(item){
         const exists = this.items.find(it => it.name === item.name);
         exists === undefined ? this.items.push({name: item.name, price: item.price, quantity: 1,}) : exists.quantity++;
+        console.log("added: ", { name: item.name, price: item.price });
+        this.total = this.setTotal();
         },
     removeItem(item){
         const exists = this.items.find(it => it.name === item.name);
         exists === undefined ? console.error('ERROR: Item not found in cart.') : exists.quantity--;
+        console.log("removed: ", { name: item.name, price: item.price });
+        this.total = this.setTotal();
     },
+    setTotal(){
+        return this.total = this.items.reduce((acc, item) => { return acc + (item.price * item.quantity)}, 0);
+    },
+    getTotal(){
+        return this.total.toFixed(2);
+    },
+    getCartList(){
+        return this.items;
+    }
 }

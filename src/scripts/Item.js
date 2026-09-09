91beb5f7d6e0e6d7c4bf4f744a99809686e38f89
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-09 17:01:04 -0500

    added quantity as default property on item object

--- src/scripts/Item.js
+++ src/scripts/Item.js
@@ -4,6 +4,7 @@ export class Item {
         this.category = category;
         this.price = price;
         this.image = image;
+        this.quantity = 0;
     }
     getTotal(qty){
         return (this.price * qty);

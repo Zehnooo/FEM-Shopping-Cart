f6467bfcb5cd09e3376a7f5e217313723fe45a9a
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-09 17:01:37 -0500

    added success and error returning in cart functions

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,20 +1,30 @@
 export const cart= {
     items: [],
     total: 0,
-
+    findItemInCart(item){
+        return cart.items.find(i => i.name === item.name) ? cart.items.find(i => i.name === item.name) : undefined;
+    },
     addItem(item){
-        const exists = cart.items.find(it => it.name === item.name);
+        const exists = cart.findItemInCart(item);
         if (exists === undefined){
-            item.quantity = 1;
+            item.quantity++;
             cart.items.push(item);
         } else { exists.quantity++; }
         cart.setTotal();
+        return { success: true, msg: `Added ${item.name} to cart` }
         },
     removeItem(item){
-        const exists = cart.items.find(it => it.name === item.name);
-        exists === undefined ? console.error('ERROR: Item not found in cart.') : exists.quantity--;
+        const exists = cart.findItemInCart(item);
+        if (exists === undefined){ return { success: false, msg: `${item.name} is not in your cart. Please try again.`} }
+        exists.quantity--;
         cart.setTotal();
+        if (exists.quantity <= 0)  {
+            cart.items = cart.items.filter(it => it.name !== item.name);
+            return { success: true, msg: `Removed ${item.name} from cart` }
+        }
+        return { success: true, msg: `Removed 1 ${item.name}` }
     },
+
     setTotal(){
         if (!cart.items.length) { cart.total = 0; }
         cart.total = (cart.items.reduce((acc, item) => { return acc + (item.price * item.quantity)}, 0));
@@ -28,5 +38,6 @@ export const cart= {
     emptyCart(){
         cart.items = [];
         cart.setTotal();
+        return { success: true, msg: 'Cart emptied...' }
     }
 }

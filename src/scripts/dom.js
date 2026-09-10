855e38145c25564d88237dd698dddfb8cec6270e
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-10 15:56:24 -0500

    started changing how the buttons are displayed on the item card depending on if there are qty added to cart or not

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -10,17 +10,18 @@ let device;
 
 export const initDom = () => {
     device = client.device.getDevice();
-    root.append(contentGrid(), toastContainer());
+    const items = getData();
+    root.append(contentGrid(items), toastContainer());
 }
 
-const contentGrid = () => {
+const contentGrid = (items) => {
     const g = newEl('div', null, 'home-grid', ['grid']);
-    g.append(dessertList(), cartList());
+    g.append(dessertList(items), cartList(items));
     return g;
 }
 
 const toastContainer = () => {
-    return newEl('div', null, 'toast-con', ['empty']);
+    return newEl('div', null, 'toast-con', []);
 }
 
 const itemCard = (item) => {
@@ -31,34 +32,39 @@ const itemCard = (item) => {
     const topCon = newEl('div', null, '', []);
     const botCon = newEl('div', null, '', []);
     const btnCon =  newEl('div', null, 'btn-con', []);
+    btnCon.dataset.id = item.getId();
 
     const fig = newEl('figure', null, '', ['img-con']);
     const img = newEl('img', null, `${item.name}-img`, ['item-img']);
     img.src = item.image[device];
 
-    const defaultAddBtn = newEl('button', 'Add to Cart', 'add-to-cart', ['default-add', 'btn']);
-    defaultAddBtn.addEventListener('click', () => {
-        const res = cart.addItem(item);
-        toast.queueToast(res.msg, res.success);
-        if (res.success) { updateCartList(); }
-    });
+    const cartCount =  newEl('span', 0, 'item-cart-count', ['cart-qty', 'no-display']);
 
-    const addBtn = newEl('button', null, 'cart-increment',  ['btn'  /*, 'no-display'*/]);
-    const subBtn = newEl('button', null, 'cart-decrement', ['btn' /*,'no-display'*/]);
+
+    const addBtn = newEl('button', null, 'cart-increment',  ['btn']);
+    const subBtn = newEl('button', null, 'cart-decrement', ['btn', 'no-display']);
 
         addBtn.addEventListener('click', () => {
             const res = cart.addItem(item);
             toast.queueToast(res.msg, res.success);
-            if (res.success) { updateCartList(); }
+            if (res.success) {
+                updateCartList();
+                updateCardQty(item);
+                updateBtnDisplay(item);
+            }
         });
-        addBtn.innerHTML= icons.increment;
+        addBtn.innerHTML= icons.increment.cart;
 
         subBtn.addEventListener('click', () => {
             const res = cart.removeItem(item);
             toast.queueToast(res.msg, res.success);
-            if (res.success) { updateCartList(); }
+            if (res.success) {
+                updateCartList();
+                updateCardQty(item);
+                updateBtnDisplay(item);
+            }
         });
-        subBtn.innerHTML = icons.decrement;
+        subBtn.innerHTML = icons.decrement.minus;
 
 
     const category = newEl('p', item.category);
@@ -66,7 +72,7 @@ const itemCard = (item) => {
     const price = newEl('p', `$${item.price.toFixed(2)}`);
 
     fig.append(img);
-    btnCon.append(subBtn, defaultAddBtn, addBtn);
+    btnCon.append(subBtn, cartCount, addBtn);
     topCon.append(fig, btnCon);
     botCon.append(category, name, price);
     con.append(topCon, botCon);
@@ -74,18 +80,18 @@ const itemCard = (item) => {
     return card;
 }
 
-const dessertList = () => {
+const dessertList = (items) => {
     const con = newEl('div', null, 'dessert-con');
     const heading = newEl('h2', 'Desserts', null, []);
 
-    const itemList = itemGrid();
+    const itemList = itemGrid(items);
 
     con.append(heading, itemList);
     return con;
 }
 
-const itemGrid = () => {
-    const items = getData();
+const itemGrid = (items) => {
+
     const g = newEl('div', null, 'item-grid', ['grid']);
 
     items.forEach(item => { g.append(itemCard(item)) });
@@ -103,22 +109,29 @@ const cartList = () => {
 
     const resetBtn = newEl('button', 'Empty Cart', 'empty-cart', ['btn']);
     resetBtn.addEventListener('click', () => {
+
         const res = cart.emptyCart();
         if (res.success){
             toast.queueToast(res.msg, res.success);
             updateCartList();
+            updateCardQty();
+            updateBtnDisplay();
         }
     });
 
     const confirmBtn = newEl('button',  'Confirm Order', 'confirm-order', ['btn']);
-    confirmBtn.addEventListener('click', () => console.log('order confirmed'));
+    confirmBtn.addEventListener('click', () => {
+        const res = cart.confirmOrder();
+        toast.queueToast(res.msg, res.success);
+        if (res.success){ const modal = orderConfirmationModal(res.order); root.append(modal); modal.showModal();  }
+    });
 
     botCon.append(total, resetBtn, confirmBtn);
     con.append(heading, listCon, botCon);
     return con;
 }
 
-const cartItem = (item) => {
+const cartItem = (item, button = false, image = false) => {
     const con = newEl('div');
 
     const name = newEl('h3', item.name);
@@ -126,14 +139,33 @@ const cartItem = (item) => {
     const qty = newEl('p', `${item.quantity}x`);
     const total = newEl('p', `$${(item.getTotal(item.quantity)).toFixed(2)}`);
 
-    const removeBtn = newEl('button', null, 'remove-item', ['btn']);
-    removeBtn.addEventListener('click',  () => {
-        const res = cart.removeAll(item);
-        toast.queueToast(res.msg, res.success);
-        if (res.success) { updateCartList(); }
-    });
+    let fig = undefined;
+    let img;
+    if (image !== false){
+        fig = newEl('figure');
+        img = newEl('img', null, null, ['img', 'thumbnail']);
+        img.src = item.image.thumbnail;
+        fig.append(img);
+    }
 
-    con.append(name, qty, price, total, removeBtn);
+    let removeBtn = undefined;
+    if (button !== false){
+        removeBtn = newEl('button', null, 'remove-item', ['btn']);
+        removeBtn.innerHTML = icons.decrement.remove;
+        removeBtn.addEventListener('click',  () => {
+            const res = cart.removeAll(item);
+            toast.queueToast(res.msg, res.success);
+            if (res.success) {
+                updateCartList();
+                updateCardQty(item);
+                updateBtnDisplay(item);
+            }
+        });
+    }
+
+    con.append(name, qty, price, total);
+    if (removeBtn !== undefined) { con.append(removeBtn); }
+    if (fig !== undefined) { con.prepend(fig); }
     return con;
 }
 
@@ -143,7 +175,7 @@ const updateCartList = () =>  {
     const list = document.querySelector('#cart-list');
     list.replaceChildren();
 
-    if (items.length > 0) {items.forEach(item => list.append(cartItem(item)));}
+    if (items.length > 0) {items.forEach(item => list.append(cartItem(item, true)));}
     updateCartTotal();
 }
 
@@ -151,3 +183,75 @@ const updateCartTotal = () => {
     document.querySelector('#cart-total').textContent = `$${cart.getTotal().toFixed(2)}`;
 }
 
+const updateCardQty = (item = null) => {
+    if (!item){
+        document.querySelectorAll('#item-cart-count').forEach(el => el.textContent = String(0));
+    } else {
+        document.querySelector(`[data-id='${item.id}']`).querySelector('#item-cart-count').textContent = String(cart.getCartQty(item));
+    }
+}
+
+const updateBtnDisplay = (item = null) => {
+
+    if (!item) {
+        document.querySelectorAll('.multi').forEach(btnCon => {
+            const dec = btnCon.querySelector('#cart-decrement');
+            const qty = btnCon.querySelector('#item-cart-count');
+            const inc = btnCon.querySelector('#cart-increment');
+            btnCon.classList.remove('multi');
+            btnCon.classList.add('single');
+            dec.classList.add('no-display');
+            qty.classList.add('no-display');
+            inc.innerHTML = icons.increment.cart;
+        });
+    }
+    const style = item.quantity <= 0 ? 'single' : 'multi';
+    const btnCon = document.querySelector(`[data-id='${item.id}']`);
+    const dec = btnCon.querySelector('#cart-decrement');
+    const qty = btnCon.querySelector('#item-cart-count');
+    const inc = btnCon.querySelector('#cart-increment');
+
+    switch(style) {
+        case 'single':
+        btnCon.classList.remove('multi');
+        btnCon.classList.add('single');
+        dec.classList.add('no-display');
+        qty.classList.add('no-display');
+        inc.innerHTML = icons.increment.cart;
+        break;
+
+        case 'multi':
+            btnCon.classList.remove('single');
+            btnCon.classList.add('multi');
+            dec.classList.remove('no-display');
+            qty.classList.remove('no-display');
+            inc.innerHTML = icons.increment.plus;
+            break;
+    }
+
+}
+
+const orderConfirmationModal = (order) => {
+    const m = newEl('dialog', null, 'order-confirmation-modal');
+    const heading = newEl('h2', 'Order Confirmed');
+    const text = newEl('p', 'We hope you enjoy your food!');
+
+    const orderCon = newEl('div');
+    const itemList = newEl('div');
+
+    order.items.forEach(item => { itemList.append(cartItem(item, false, true)); });
+
+    const newOrderBtn = newEl('button', 'Start New Order', 'new-order', ['btn']);
+    newOrderBtn.addEventListener('click', () => {
+        m.close();
+        cart.emptyCart();
+        updateCartList();
+        updateCardQty();
+    });
+
+    const orderTotal = newEl('h4', `$${order.total.toFixed(2)}`);
+
+    orderCon.append(itemList, orderTotal);
+    m.append(heading, text, orderCon, newOrderBtn);
+    return m;
+}

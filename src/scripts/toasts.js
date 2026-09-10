bc34a2bd5505f370e9d74d141185a7e9e42a47fa
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-10 11:58:36 -0500

    updated toast styles; added toast auto-removal from DOM and queue

--- src/scripts/toasts.js
+++ src/scripts/toasts.js
@@ -3,18 +3,47 @@ import { newEl } from "./elements.js";
 export const toast = {
     queue: [],
     createMsg(txt, type){
-        return {txt, type}
+        return {txt, type, count: 1}
     },
     printMsg(msg){
-        document.querySelector('#toast-con').append(newEl('p', msg.txt, null, [`${msg.type}`, 'toast']));
+        let counter;
+
+        const queue = document.querySelector('#toast-con');
+        const newMsg = newEl('p', `${msg.txt}`, null, [`${msg.type}`, 'toast']);
+
+        msg.count > 1 ? counter = newEl('span', ` ${msg.count}x`) : counter = undefined;
+        if (counter !== undefined) { newMsg.append(counter); toast.removeMsg(msg); }
+
+        queue.prepend(newMsg);
         toast.toggleQueue();
+        setTimeout(() => {
+            newMsg.remove();
+            toast.queue = toast.queue.filter(m => m.txt !== msg.txt);
+        }, 3000);
+    },
+    removeMsg(msg){
+        const queue = document.querySelector('#toast-con');
+        const allMsgs = queue.querySelectorAll('p');
+        if (!allMsgs.length) return;
+        const toRemove = [...allMsgs].filter(m => m.textContent.includes(msg.txt)) || [];
+        if (toRemove.length > 0) { toRemove.forEach(el => el.remove()); }
+
     },
     findMsgInQueue(msg){
-        return toast.queue.find(m => m.message === msg.message);
+        return toast.queue.find(m => m.txt === msg.txt) || undefined;
     },
-    queueToast(txt, type){
-    toast.queue.push(txt);
-    toast.printMsg(toast.createMsg(txt, type));
+    queueToast(txt, status){
+    const type = (status === true ? "success" : "error");
+    const msg = toast.createMsg(txt, type);
+    const exists = toast.findMsgInQueue(msg);
+
+    if (exists === undefined) {
+        toast.queue.push(msg);
+        toast.printMsg(msg);
+    } else {
+        exists.count++;
+        toast.printMsg(exists);
+    }
     },
     toggleQueue(){
          const queue = document.querySelector('#toast-con');
@@ -24,4 +53,5 @@ export const toast = {
     isQueueEmpty(){
         return !toast.queue.length;
     }
+
 }
